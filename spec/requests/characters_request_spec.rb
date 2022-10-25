52eb86e3ab261fffd300ca14590f3fc5438cc83c
let <kyle.tran.le@gmail.com>
2022-10-25 14:48:38 -0700

    saving work

---
+++ spec/requests/characters_request_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe "Characters", type: :request do
+  describe "GET /index" do
+    it "gets a list of characters" do
+      Character.create(
+        name: 'Felix',
+        age: 2,
+        enjoys: 'Walks in the park',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+      )
+
+      # Make a request
+      get '/characters'
+
+      character = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(character.length).to eq 1
+    end
+  end
+end 
+
+  describe "POST /create" do
+    it "creates a character" do
+      # The params we are going to send with the request
+      character_params = {
+        character: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+  
+      # Send the request to the server
+      post '/characters', params: character_params
+  
+      # Assure that we get a success back
+      expect(response).to have_http_status(200)
+  
+      # Look up the character we expect to be created in the db
+      character = Character.first
+  
+      # Assure that the created character has the correct attributes
+      expect(character.name).to eq 'Buster'
+    end
+  end
--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
+require 'rspec'
+require_relative 'rspec'
 
 RSpec.describe "Characters", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it "should get the list of characters" do 
+    get 'characters'
+
+    character = JSON.parse(response.body)
   end
 end
